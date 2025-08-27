812b22cbaf08c25f05dd2891b30968d9b2bcdf21
Azure-AWS-Cloud <8984506+azure-aws-cloud@users.noreply.github.com>
2025-08-27 12:00:01 +0530

    build number and timezone can be passed from cmd line

--- src/main/java/com/rhushi/BuildInfo.java
+++ src/main/java/com/rhushi/BuildInfo.java
@@ -2,19 +2,11 @@ package com.rhushi;
 
 
 import java.io.InputStream;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.Properties;
-import java.util.TimeZone;
 
 public class BuildInfo {
 
-    public static void main(String[] args) {
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd h:mm:ss a z");
-        sdf.setTimeZone(TimeZone.getTimeZone("America/New_York"));
-        System.out.println("Build Timestamp: " + sdf.format(new Date()));
-    }
-    public static void main1(String[] args) {
+   public static void main(String[] args) {
         Properties properties = new Properties();
 
         try (InputStream input = BuildInfo.class
